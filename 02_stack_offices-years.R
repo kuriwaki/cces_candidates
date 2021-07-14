6e2aa58cc932e270c3f1619be552495f329936d8
cha <jeremiahcha97@gmail.com>
2021-07-14 19:05:58 -0400

    Filling 02_stack_offices-yrs.R

--- 02_stack_offices-years.R
+++ 02_stack_offices-years.R
@@ -1,3 +1,23 @@
-library(haven)
 
-jksk <- read_dta("~/Dropbox/CCES_candidates_Dropbox/Release/cces_candidates_pre.dta")
+# Frontmatter -------------------------------------------------------------
+
+library(haven)
+library(tidyverse)
+
+setwd("/Users/jeremiahcha/Desktop/github/cces_candidates/")
+
+
+# Loading in data ---------------------------------------------------------
+
+# Congressional and Gubernatorial Candidates (2006-2016)
+cand_16 <- read_dta("data/snyder/2020-06-13 election_results_2006_2016.dta")
+# Congressional and Gubernatorial Candidates (2018)
+cand_18 <- read_dta("data/snyder/2020-08-16 election_results_2018_names_updated.dta")
+# Congressional and Gubernatorial Candidates (2020)
+cand_20 <- read_dta("data/intermediate/2020_uspresident-congress_JC-SK.dta")
+
+
+
+# Binding data ------------------------------------------------------------
+
+cand <- bind_rows(cand_16, cand_18, cand_20)
