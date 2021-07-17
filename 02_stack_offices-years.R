c85b14493c4287d02a43a22f24f305cc579ae7e2
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-07-17 10:08:56 -0400

    Make 02 the place for renaming, stacking, etc..

--- 02_stack_offices-years.R
+++ 02_stack_offices-years.R
@@ -4,7 +4,9 @@
 library(haven)
 library(tidyverse)
 
-setwd("/Users/jeremiahcha/Desktop/github/cces_candidates/")
+if (Sys.info()["user"] == "jeremiahcha") {
+  setwd("/Users/jeremiahcha/Desktop/github/cces_candidates/")
+}
 
 
 # Loading in data ---------------------------------------------------------
@@ -18,6 +20,21 @@ cand_20 <- read_dta("data/intermediate/2020_uspresident-congress_JC-SK.dta")
 
 
 
-# Binding data ------------------------------------------------------------
+# Binding data --------
 
-cand <- bind_rows(cand_16, cand_18, cand_20)
+cand_all <- bind_rows(cand_16, cand_18, cand_20) %>%
+  arrange(year, office, state, dist)
+
+# Limit to necessary and rename ------
+cand <- cand_all %>%
+  select(year, state, office, dist, type, nextup,
+         party, name,
+         inc, cand_all = vote_g, won = w_g)
+
+
+# Add variable labels -----
+
+
+# Release Dataverse version
+write_dta(cand, "release/candidates_2006-2020.dta")
+
