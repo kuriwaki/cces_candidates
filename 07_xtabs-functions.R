9be7a179670f0464a58fd5cf1e5844ee429da693
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-07-25 22:48:49 -0400

    Move from dropbox, adapt to guides format, separate by dataset

---
+++ 07_xtabs-functions.R
@@ -0,0 +1,69 @@
+library(haven)
+library(fs)
+library(knitr)
+library(glue)
+library(rcces) # install from remotes::install_github("kuriwaki/rcces")
+library(kableExtra)
+library(tidyverse)
+
+
+# functions ---
+fmt_xtab <- function(xtbl, name) {
+  tab <- kable(xtbl,
+               format = "latex",
+               booktabs = TRUE,
+               linesep = "",
+               format.args = list(big.mark = ',')) %>%
+    add_header_above(header = c("year" = 1, XXXXX = ncol(xtbl))) %>%
+    kable_styling(position = "center")
+
+  str_replace(tab, "XXXXX", name)
+}
+
+wri_xtab <- function(tbl, name, dir = "guide/Tables") {
+  tbl %>%
+    str_remove("\\\\begin\\{table\\}\\[(H|t)\\]\\n") %>%
+    str_remove("\\\\centering\\n") %>%
+    str_remove("\\\\caption\\{NA\\}\\n") %>%
+    str_remove("\\n\\\\end\\{table\\}") %>%
+    str_replace("\\\\begin\\{tabular\\}", "\\\\footnotesize\\\\begin{tabular}[t]") %>%
+    write_lines(glue("{dir}/{name}.tex"))
+}
+
+write_numbers <- function(tbl, office_lbl, ndir = "guide/Ns") {
+  tbl %>%
+    distinct(office, st, year, dist_up) %>%
+    nrow() %>%
+    rcces::cmfmtW(file = path(ndir, glue("{office_lbl}_dists.tex")))
+
+  tbl %>%
+    distinct(office, st, year, name_snyder) %>%
+    nrow() %>%
+    rcces::cmfmtW(file = path(ndir, glue("{office_lbl}_cands.tex")))
+}
+
+# Import data ----
+release_dir <- "~/Dropbox/CCES_candidates/Release/"
+
+
+
+
+
+# Counts -- respondents
+count_rdat <- function(tbl, name) {
+  tbl %>%
+  mutate(year = factor(as.character(year), levels = as.character(seq(2006, 2018, 2)))) %>%
+  group_by(year, .drop = FALSE) %>%
+  summarize(n_respondents = n_distinct(case_id),
+            n_nonmissing = sum(name_snyder != ""),
+            n_rows = n(),
+            .groups = "drop") %>%
+  kable(format = "latex", booktabs = TRUE,
+        format.args = list(big.mark = ","),
+        col.names = c("Year", "Respondents", "Non-Missing", "Total"),
+        linesep = "") %>%
+    add_header_above(c(" " = 2, "\\\\shortstack{Respondent $\\\\times$\\\\\\\\ Office $\\\\times$ Candidate Pairs}" = 2),
+                     escape = FALSE) %>%
+    wri_xtab(name)
+}
+
---
+++ 07a_xtabs-cand.R
@@ -0,0 +1,54 @@
+source("07_xtabs-functions.R")
+
+
+jsdat <- read_dta("release/candidates_2006-2020.dta") %>%
+  filter(office %in% c("S", "H", "G"), # temp before filtering
+         year %% 2 == 0)
+
+
+# counts JS ----
+jsdat %>% filter(party %in% c("D", "R")) %>%  write_numbers("all")
+jsdat %>% filter(office == "G", party %in% c("D", "R")) %>% write_numbers("gov")
+jsdat %>% filter(office == "H", party %in% c("D", "R")) %>% write_numbers("house")
+jsdat %>% filter(office == "S", party %in% c("D", "R")) %>% write_numbers("sen")
+
+
+# var example
+
+jsdat %>%
+  filter(year == 2016, state == "MO",  (office %in% c("S", "G") | dist == 5)) %>%
+  mutate(party = fct_relevel(party, "D", "R"),
+         office = fct_relevel(factor(office), "H", "S", "G")) %>%
+  arrange(office, party) %>%
+  select(-party_formal, -type) %>%
+  mutate(across(c(year), as_factor)) %>%
+  kable(format = "latex", booktabs = TRUE,
+        format.args = list(big.mark = ","),
+        linesep = "") %>%
+  column_spec(seq_len(10), monospace = TRUE)  %>%
+  write_lines("guides/Tables/example_candidates.tex")
+
+
+
+# main xtab
+xtabs(~ year + office, jsdat) %>%
+  fmt_xtab("office") %>%
+  wri_xtab("office_by-cand")
+
+
+
+jsdat  %>%
+  mutate(party_lump = fct_lump(party_short, n = 5, other_level = "Other"),
+         party_lump = fct_relevel(party_lump, "D", "R", "I", "Lbt", "Grn", "")) %>%
+  xtabs(~ year + party_lump, .) %>%
+  fmt_xtab("party") %>%
+  wri_xtab("party_by-cand")
+
+
+
+
+# incumbent
+jsdat %>%
+  xtabs(~ year + inc, .) %>%
+  fmt_xtab("inc") %>%
+  wri_xtab("inc_by-cand")
