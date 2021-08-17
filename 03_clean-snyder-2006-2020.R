bf63941f1fc87af5957badff7d265af4daf95777
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-08-17 11:34:33 -0400

    consistent with past name

--- 03_clean-snyder-2006-2020.R
+++ 03_clean-snyder-2006-2020.R
@@ -8,11 +8,11 @@ jsdat_all <- read_dta("data/snyder/2021-07-29 sen_gov_house_2006_2020.dta")
 jsdat <- jsdat_all %>%
   # ONLY KEEP GENERALS in three offices
   filter(office %in% c("S", "H", "G"), year %% 2 == 0) %>%
-  mutate(party_full = str_squish(party),
-         party = recode(party_full, `Democrat` = "D", `Republican` = "R"),
+  mutate(party_formal = str_squish(party),
+         party = recode(party_formal, `Democrat` = "D", `Republican` = "R"),
          party = fct_lump_n(party, n = 6)
   )
 
 
-jsdat %>% filter(party == "Other") %>% count(party_full, sort=  TRUE)
+jsdat %>% filter(party == "Other") %>% count(party_formal, sort=  TRUE)
 
