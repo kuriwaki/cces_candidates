cd225ac388e0c6f1f20ce6beb1206d6cf32cc00b
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-08-23 13:03:44 -0400

    Stack 2020 Governor

--- 03_clean-snyder-2006-2020.R
+++ 03_clean-snyder-2006-2020.R
@@ -1,6 +1,6 @@
 source("07_xtabs-functions.R")
 
-
+gov_2020 <- read_dta("data/intermediate/2020_govstateexec.dta")
 jsdat_all <- read_dta("data/snyder/2021-07-29 sen_gov_house_2006_2020.dta")
 
 
@@ -8,11 +8,14 @@ jsdat_all <- read_dta("data/snyder/2021-07-29 sen_gov_house_2006_2020.dta")
 jsdat <- jsdat_all %>%
   # ONLY KEEP GENERALS in three offices
   filter(office %in% c("S", "H", "G"), year %% 2 == 0) %>%
+  bind_rows(filter(gov_2020, year == 2020, office == "G")) %>%
   mutate(party_formal = str_squish(party),
          party = recode(party_formal, `Democrat` = "D", `Republican` = "R"),
          party = fct_lump_n(party, n = 6)
   )
 
 
-jsdat %>% filter(party == "Other") %>% count(party_formal, sort=  TRUE)
+jsdat %>% filter(party == "Other") %>%
+  count(party_formal, sort = TRUE) %>%
+  print(n = 336)
 
