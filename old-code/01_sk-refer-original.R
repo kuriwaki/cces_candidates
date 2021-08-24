c185f081d10510dc6eee2ee55bd81c2f0e185201
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-08-24 18:38:40 -0400

    Superseded by 2021-07-29 sen_gov_house_2006_2020

---
+++ old-code/01_sk-refer-original.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(haven)
+cand <- read_dta("~/Dropbox/elections/data/input/snyder/tmp_H_1950_1980.dta")
+cand <- read_dta("~/Dropbox/elections/data/input/snyder/js_2020-11-03_statewide_1950-2018.dta")
+cand <- read_dta("~/Dropbox/CCES_candidates_Dropbox/Release/candidates_snyder.dta")
+
+
+cand %>%
+  filter(st %in% "OR") %>%
+  # filter(party %in% c("D", "R")) %>%
+  filter(str_detect(name_snyder, regex("cours", ignore_case = TRUE))) %>%
+  select(year, st, di = dist_up, office, name_snyder, party, inc)
