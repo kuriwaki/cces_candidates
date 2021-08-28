75703b8c8415c0fc7795151bbd7d7b3c0c3901ce
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-08-28 10:44:14 -0700

    separate 03 into 02 (correct) and 03 (stack)

---
+++ 02_correct-snyder-data.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(haven)
+
+# raw
+jsdat_raw <- read_dta("data/snyder/2021-07-29 sen_gov_house_2006_2020.dta")
+
+
+# issue 7
+jsdat <- jsdat_raw %>%
+  mutate(
+    type = replace(
+      x = type,
+      list = year == 2006 & state == "TX" & dist == 22 & nextup == 2006,
+      values = "S"),
+    type = replace(
+      x = type,
+      list = year == 2006 & state == "TX" & dist == 22 & nextup == 2008,
+      values = "G")
+    )
+
+
+# write
+write_rds(jsdat, "data/intermediate/candidates_2006-2020.rds")
--- 03_stack-sources.R
+++ 03_stack-sources.R
@@ -1,15 +1,17 @@
-source("07_xtabs-functions.R")
+library(tidyverse)
+library(haven)
 
-gov_2020 <- read_dta("data/intermediate/2020_govstateexec.dta")
-jsdat_all <- read_dta("data/snyder/2021-07-29 sen_gov_house_2006_2020.dta")
-jsdat_all %>% filter(str_detect(name, "OSSOFF"))
+# read data ----
+jsdat_all <- read_rds("data/intermediate/candidates_2006-2020.rds")
+gov_2020 <- read_csv("data/intermediate/2020_gov.csv")
 
 
+# filter, stack, modify ------
 jsdat <- jsdat_all %>%
-  # ONLY KEEP GENERALS in three offices
+  # ONLY keep three offices
   filter(office %in% c("S", "H", "G")) %>%
   # ADD GOVERNOR
-  bind_rows(filter(gov_2020, year == 2020, office == "G")) %>%
+  bind_rows(gov_2020) %>%
   # PARTY EDITS
   mutate(party_formal = str_squish(party),
          party = fct_lump_n(party, n = 6),
@@ -19,7 +21,7 @@ jsdat <- jsdat_all %>%
   mutate(
     candidatevotes = coalesce(candidatevotes, vote_g),
     won = coalesce(won, w_g)) %>%
-  select(-vote_g, -w_g, -u_g, -name_orig) %>%
+  select(-vote_g, -w_g, -u_g) %>%
   # TOTAL VOTE
   group_by(year, office, state, dist, type) %>%
   mutate(totalvotes = sum(candidatevotes)) %>%
@@ -29,5 +31,5 @@ jsdat <- jsdat_all %>%
 
 
 
-# Out
+# Save -----
 write_rds(jsdat, "data/intermediate/candidates_2006-2020.rds")
