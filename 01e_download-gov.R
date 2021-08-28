1e666e3118f59f93cfbea53303d76698123a43fe
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-08-28 10:43:59 -0700

    Track governor 2020 data (it's small enough)

--- 01e_download-gov.R
+++ 01e_download-gov.R
@@ -6,7 +6,7 @@ g2020_gov <- range_read(g2020_ss, sheet = "State-Executive")
 
 # rename for other data
 g2020_std <- g2020_gov %>%
-  rename(state = st)
+  rename(state = st) %>%
+  filter(office == "G")
 
-
-write_dta(g2020_std, "data/intermediate/2020_govstateexec.dta")
+write_csv(g2020_std, "data/intermediate/2020_gov.csv")
