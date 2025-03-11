619f719c2d71ecf50e36a9f2001ed104df95c47e
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2025-03-11 15:14:34 -0400

    Wrong variable name. Switch to case_match to avoid repetition?

--- 08_issuefix.R
+++ 08_issuefix.R
@@ -66,19 +66,18 @@ cand <- cand |>
     name_snyder = replace(name_snyder, name_snyder == "WAKELY, TOMMY", "WAKELY, THOMAS J. (TOM)")
   )
 
-# Fixing Issue # 43
-
-name_corrections <- c(
-  "CUELLAR, HENRY" = "CUELLAR, ENRIQUE ROBERTO (HENRY)",
-  "SHERRILL, MIKIE" = "SHERRILL, REBECCA MICHELLE (MIKIE)",
-  "KELLY, TRENT" = "KELLY, JOHN TRENT",
-  "GARCIA, CHUY" = "GARCIA, JESUS G. (CHUY)",
-  "SCOTT, JAMES AUSTIN" = "SCOTT, AUSTIN",
-  "FERGUSON, DREW" = "FERGUSON, ANDREW DREW, IV",
-  "STEUBE, GREG" = "STEUBE, W. GREGORY (GREG)"
-)
-
+# Manual recodes (#43)
 cand <- cand %>%
-  mutate(name = ifelse(name %in% names(name_corrections), name_corrections[name], name))
+  tidylog::mutate(name_snyder = case_match(
+    name_snyder,
+    "CUELLAR, HENRY" ~ "CUELLAR, ENRIQUE ROBERTO (HENRY)",
+    "SHERRILL, MIKIE" ~ "SHERRILL, REBECCA MICHELLE (MIKIE)",
+    "KELLY, TRENT" ~ "KELLY, JOHN TRENT",
+    "GARCIA, CHUY" ~ "GARCIA, JESUS G. (CHUY)",
+    "SCOTT, JAMES AUSTIN" ~ "SCOTT, AUSTIN",
+    "FERGUSON, DREW" ~ "FERGUSON, ANDREW DREW, IV",
+    "STEUBE, GREG" ~ "STEUBE, W. GREGORY (GREG)",
+    .default = name_snyder
+  ))
 
 write_rds(cand, "data/intermediate/candidates_2006-2022.rds")
